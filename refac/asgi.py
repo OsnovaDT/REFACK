0a66346d41a88eb796f43831bd2df142469fb9b4
e.stepanov <e.stepanov@justhost.ru>
2022-06-07 22:41:50 +0300

    Refactoring of refac app

--- refac/asgi.py
+++ refac/asgi.py
@@ -1,16 +1,10 @@
-"""
-ASGI config for refac project.
+"""ASGI config of project"""
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
-import os
+from os import environ
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refac.settings')
+
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'refac.settings')
 
 application = get_asgi_application()
--- refac/settings.py
+++ refac/settings.py
@@ -1,26 +1,18 @@
 """Settings of project"""
 
+from os import path
 from pathlib import Path
 
-import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# IMPORTANT
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-dnwd2-kld#p*9p_6p#*bknforuoiz&48$+v8jc7l70rfe!^$cc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -59,12 +51,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'refac.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -72,28 +58,7 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
+# INTERNATIONALIZATION
 
 LANGUAGE_CODE = 'en-us'
 
@@ -103,13 +68,21 @@ USE_I18N = True
 
 USE_TZ = True
 
+# STATIC
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    path.join(BASE_DIR, 'static/'),
 )
+
+# MEDIA
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
+
+# LOGIN AND LOGOUT
 
 LOGIN_REDIRECT_URL = 'refactoring:index'
 
@@ -117,12 +90,27 @@ LOGOUT_REDIRECT_URL = 'account:login'
 
 LOGIN_URL = 'account:login'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# OTHER
 
-STATIC_URL = '/static/'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
+    },
+]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+WSGI_APPLICATION = 'refac.wsgi.application'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
