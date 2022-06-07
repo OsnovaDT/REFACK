96bb40579df09019d359cc278b4bd6e3663e3480
e.stepanov <e.stepanov@justhost.ru>
2022-06-07 22:29:14 +0300

    Add views for refactoring app

---
+++ refactoring/urls.py
@@ -0,0 +1,45 @@
+"""Urls of refactoring app"""
+
+from django.urls import path
+
+from refactoring.views import (
+    ManualCodeInputView, IndexView, refactor_code_handler,
+    RefactoringResultsView, InstructionView, RulesView,
+)
+
+
+app_name = 'refactoring'
+
+urlpatterns = [
+    path(
+        '',
+        IndexView.as_view(),
+        name='index',
+    ),
+
+    path(
+        'manual_input',
+        ManualCodeInputView.as_view(),
+        name='manual_input',
+    ),
+    path(
+        'refactor',
+        refactor_code_handler,
+        name='refactor',
+    ),
+    path(
+        'results',
+        RefactoringResultsView.as_view(),
+        name='results',
+    ),
+    path(
+        'instruction',
+        InstructionView.as_view(),
+        name='instruction',
+    ),
+    path(
+        'rules',
+        RulesView.as_view(),
+        name='rules',
+    ),
+]
--- refactoring/views.py
+++ refactoring/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+"""Views of refactoring app"""
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import HttpResponse
+
+
+class ManualCodeInputView(TemplateView):
+    """View for manual code input"""
+
+    template_name = 'manual_code_input.html'
+
+
+class IndexView(TemplateView):
+    """View for index page"""
+
+    template_name = 'index.html'
+
+
+def refactor_code_handler(request: WSGIRequest) -> HttpResponse:
+    """Handler for code refactoring"""
+
+    return render(
+        request,
+        'refactoring_results.html',
+        {'results': 'TODO'},
+    )
+
+
+class InstructionView(TemplateView):
+    """View for instruction page"""
+
+    template_name = 'instruction.html'
+
+
+class RulesView(TemplateView):
+    """View for rules page"""
+
+    template_name = 'rules.html'
+
+
+class RefactoringResultsView(TemplateView):
+    """View for refactoring results page"""
+
+    template_name = 'refactoring_results.html'
