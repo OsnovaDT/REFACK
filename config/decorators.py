f0b974ed62311151349897b2a9ecf4e3e7f88291
e.stepanov <maestrojane@gmail.com>
2022-07-23 03:07:12 +0300

    REFACK-43: Add first exceptions handler

---
+++ config/decorators.py
@@ -0,0 +1,25 @@
+"""Decorators of the project"""
+
+from functools import wraps
+
+from django.http import JsonResponse
+from loguru import logger
+
+
+def catch_json_response_exception(func):
+    """Decorator for catching exception in JsonResponse view"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            response = func(*args, **kwargs)
+        except Exception as exception:
+            logger.error(exception)
+
+            response = JsonResponse({
+                'error': 'Произошла внутренняя ошибка сервера',
+            })
+
+        return response
+
+    return wrapper
--- refactoring/views.py
+++ refactoring/views.py
@@ -6,11 +6,22 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import FileResponse, JsonResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from loguru import logger
 
+from config.decorators import catch_json_response_exception
 from refactoring.models import RefactoringRecommendation
 from refactoring.services import (
     get_recommendations_or_error_response, create_refactoring_recommendation,
     get_file_response_with_refactoring_recommendations,
+)
+
+
+logger.add(
+    f"logs/{__name__}.log",
+    format="{level} {time} {message}",
+    level="ERROR",
+    rotation="10 KB",
+    compression="zip",
 )
 
 
@@ -41,7 +52,8 @@ class RulesView(LoginRequiredMixin, TemplateView):
 # Refactoring
 
 
-@login_required()
+@login_required
+@catch_json_response_exception
 def refactor_code_view(request: WSGIRequest) -> JsonResponse:
     """Refactor code and return recommendations or error"""
 
@@ -66,7 +78,7 @@ class RefactoringRecommendationListView(LoginRequiredMixin, ListView):
         )
 
 
-@login_required()
+@login_required
 def save_recommendation_view(request: WSGIRequest) -> JsonResponse:
     """Save refactoring recommendation for the user"""
 
@@ -84,7 +96,7 @@ def save_recommendation_view(request: WSGIRequest) -> JsonResponse:
     return JsonResponse({})
 
 
-@login_required()
+@login_required
 def download_recommendations_json_view(request: WSGIRequest) -> JsonResponse:
     """Download JSON file with refactoring recommendations"""
 
@@ -93,7 +105,7 @@ def download_recommendations_json_view(request: WSGIRequest) -> JsonResponse:
     )
 
 
-@login_required()
+@login_required
 def download_recommendations_pdf_view(request: WSGIRequest) -> FileResponse:
     """Download PDF file with refactoring recommendations"""
 
@@ -102,7 +114,7 @@ def download_recommendations_pdf_view(request: WSGIRequest) -> FileResponse:
     )
 
 
-@login_required()
+@login_required
 def download_recommendations_xml_view(request: WSGIRequest) -> FileResponse:
     """Download XML file with refactoring recommendations"""
 
